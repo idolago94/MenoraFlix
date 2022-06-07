35f7b1c4e9ef4bcf2ee5b8987899cb7cb3c96b50
idol <idol@moonsite.co.il>
2022-06-08 01:36:17 +0300

    tabs icons

--- src/routes/TabNavigator.js
+++ src/routes/TabNavigator.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Image, StyleSheet } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home, Favourite } from '../screens';
 import { Colors } from '../utils/Enums';
 import { connect } from 'react-redux';
 import { intersection } from 'lodash';
+import imgSrc from '../utils/Images'
 
 const Tab = createBottomTabNavigator();
 
@@ -14,14 +16,32 @@ const TabsNavigator = (props) => {
             screenOptions={{
                 headerShown: false,
                 tabBarStyle: { backgroundColor: Colors.SCREEN_BG },
-                tabBarInactiveTintColor: Colors.PRIMARY
+                tabBarInactiveTintColor: Colors.PRIMARY,
             }}
         >
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Favourite" component={Favourite} options={{ tabBarBadge: favCount > 0 ? favCount : null }} />
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{ tabBarIcon: () => <Image style={s.tabIcon} source={imgSrc.home} /> }}
+            />
+            <Tab.Screen
+                name="Favourite"
+                component={Favourite}
+                options={{
+                    tabBarBadge: favCount > 0 ? favCount : null,
+                    tabBarIcon: () => <Image style={s.tabIcon} source={imgSrc.star} />
+                }}
+            />
         </Tab.Navigator>
     )
 }
+
+const s = StyleSheet.create({
+    tabIcon: {
+        height: 30,
+        width: 30
+    }
+})
 
 const mapStateToProps = state => {
     return {
